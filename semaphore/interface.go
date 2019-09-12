58b484806b956c529171c0c557c75136bcb695b8
Markus Thömmes <markusthoemmes@me.com>
2019-09-12 09:46:51 +0200

    Complete the interface.

--- semaphore/interface.go
+++ semaphore/interface.go
@@ -5,6 +5,7 @@ import "context"
 type Interface interface {
 	// Acquire gets a token.
 	Acquire(context.Context) error
+	// TryAcquire returns true if it can get a token immediately.
 	TryAcquire() bool
 	// Release returns a token.
 	Release()
